268fc5d7410c85e37c739d8de04b15bd920de052
Abhishek <abbybagul646@gmail.com>
2023-01-11 14:53:02 +0530

    Managing Classes & Admin Login

---
+++ LeanerAcademyProject/src/main/java/com/LearnerAcademy/createClass/createClass.java
@@ -0,0 +1,85 @@
+package com.LearnerAcademy.createClass;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Servlet implementation class createClass
+ */
+public class createClass extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public createClass() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		try {
+			
+			HttpSession session=request.getSession();
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/LearnersAcademy", "root", "root12345");
+		
+			//check if user has permission
+			String role = (String)session.getAttribute("role");	
+			
+			if(role != "admin"){
+				response.sendRedirect("admin.jsp");
+				return;
+			}
+			
+			//input values
+			String className = request.getParameter("className");
+			String stdName = request.getParameter("stdName");
+			String divName = request.getParameter("divName");
+			
+			//retrive values from db
+			PreparedStatement ps = con.prepareStatement("INSERT INTO class (name,class,std) VALUES (?,?,?)");
+			ps.setString(1, className);
+			ps.setString(2, divName);
+			ps.setString(3, stdName);
+			int i = ps.executeUpdate();
+			
+			if(i > 0) {
+				response.sendRedirect("manageClasses/createClass.jsp?action=creation&status=1");
+			}else{
+				response.sendRedirect("manageClasses/createClass.jsp?action=creation&status=0&className=" + className + "&stdName="+stdName + "&divName="+divName);
+			}
+			
+			
+		} catch (SQLException e) {	;
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ LeanerAcademyProject/src/main/java/com/learnersacademy/helloservlet/HelloServlet.java
@@ -0,0 +1,47 @@
+package com.learnersacademy.helloservlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw = response.getWriter();
+		String s = request.getParameter("t1");
+		pw.println("<h1>Hello, "+s+"</h1>");
+		pw.close();
+		
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
